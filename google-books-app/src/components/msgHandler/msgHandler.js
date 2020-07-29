42c704c846bcb75b1b7784cc9e1fb33395c3abc5
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-07-28 20:36:37 -0700

    fixed message margin on msghandler

--- google-books-app/src/components/msgHandler/msgHandler.js
+++ google-books-app/src/components/msgHandler/msgHandler.js
@@ -1,8 +1,9 @@
 import React from "react"
+import "./msgHandler.css"
 export default function Message(props){
     
     return(
-        <div className="col-12">
+        <div className="col-12 messages">
             {props.message}
         </div>
     )
